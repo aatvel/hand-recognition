781f374b4872683af054f96b9869b648ff3db0ba
Alina <alina.andrevna3993@gmail.com>
2023-01-31 20:23:08 +0300

    added rock

--- src/utilities.js
+++ src/utilities.js
@@ -9,27 +9,27 @@ const fingerJoints = {
   
   // Infinity Gauntlet Style
   const style = {
-    0: { color: "DarkTurquoise", size: 15 },
-    1: { color: "Aquamarine", size: 6 },
-    2: { color: "Navy", size: 10 },
-    3: { color: "Aquamarine", size: 6 },
-    4: { color: "Aquamarine", size: 6 },
+    0: { color: "DarkTurquoise", size: 8 },
+    1: { color: "Aquamarine", size: 4 },
+    2: { color: "Navy", size: 7 },
+    3: { color: "Aquamarine", size: 4 },
+    4: { color: "Aquamarine", size: 4 },
     5: { color: "MediumBlue", size: 10 },
-    6: { color: "Aquamarine", size: 6 },
-    7: { color: "Aquamarine", size: 6 },
-    8: { color: "Aquamarine", size: 6 },
-    9: { color: "blue", size: 10 },
-    10: { color: "Aquamarine", size: 6 },
-    11: { color: "Aquamarine", size: 6 },
-    12: { color: "Aquamarine", size: 6 },
-    13: { color: "DeepSkyBlue", size: 10 },
-    14: { color: "Aquamarine", size: 6 },
-    15: { color: "Aquamarine", size: 6 },
-    16: { color: "Aquamarine", size: 6 },
-    17: { color: "Aqua", size: 10 },
-    18: { color: "Aquamarine", size: 6 },
-    19: { color: "Aquamarine", size: 6 },
-    20: { color: "Aquamarine", size: 6 },
+    6: { color: "Aquamarine", size: 4 },
+    7: { color: "Aquamarine", size: 4 },
+    8: { color: "Aquamarine", size: 4 },
+    9: { color: "blue", size: 7 },
+    10: { color: "Aquamarine", size: 4 },
+    11: { color: "Aquamarine", size: 4 },
+    12: { color: "Aquamarine", size: 4 },
+    13: { color: "DeepSkyBlue", size: 7 },
+    14: { color: "Aquamarine", size: 4 },
+    15: { color: "Aquamarine", size: 4 },
+    16: { color: "Aquamarine", size: 4 },
+    17: { color: "Aqua", size: 7 },
+    18: { color: "Aquamarine", size: 4 },
+    19: { color: "Aquamarine", size: 4 },
+    20: { color: "Aquamarine", size: 4 },
   };
   
   // Drawing function
@@ -61,7 +61,7 @@ const fingerJoints = {
               landmarks[secondJointIndex][1]
             );
             ctx.strokeStyle = "Ivory";
-            ctx.lineWidth = 4;
+            ctx.lineWidth = 2;
             ctx.stroke();
           }
         }
@@ -74,7 +74,7 @@ const fingerJoints = {
           const y = landmarks[i][1];
           // Start drawing
           ctx.beginPath();
-          ctx.arc(x, y, style[i]["size"], 0, 3 * Math.PI);
+          ctx.arc(x, y, style[i]["size"], 0, 2 * Math.PI);
   
           // Set line color
           ctx.fillStyle = style[i]["color"];
